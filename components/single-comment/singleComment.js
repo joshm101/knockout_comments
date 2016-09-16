514ef7f9ef2f1f70110fd1ea2488fd61890c99b8
Joshua Mora <jshm101@gmail.com>
2016-09-16 03:19:48 -0700

    Comments with voting capabilities. Pretty basic. Could not make it so that the vote buttons are hidden until you hover over the comment because for some reason, the mouseover event is being fired when the comment element is first loaded...

--- components/single-comment/singleComment.js
+++ components/single-comment/singleComment.js
@@ -1,19 +1,45 @@
-define(["../../js/knockout/knockout-3.4.0"], function(ko) {
-   function singleCommentViewModel() {
-      this.num = 4;
+
+
+require.config( {
+    baseUrl : '.'
+});
+
+define(["../../js/require/require", "../../js/knockout/knockout-3.4.0"], function(rq, ko) {
+   function singleCommentViewModel(params) {
+      this.username = params.username;
+      this.date = params.date;
+      this.commentText = params.commentText;
+      this.score = params.score
+      this.lastVoteButtons;
+
+      this.upvote = function() {
+         this.score(this.score() + 1);
+      };
+      
+      this.downvote = function() {
+         this.score(this.score() - 1); 
+      };
+      
+      /*
+      
+      this.enableVoteButtons = function(el, event) {
+         console.log(el);
+         console.log(event);
+         el.children[4].style.setProperty('visibility', 'visible', 'important');
+         this.lastVoteButtons = el.children[4];
+      };
+      
+      this.disableVoteButtons = function(el) {
+         this.lastVoteButtons.style.setProperty('visibility', 'hidden', 'important');
+      };*/
    };
 
    return {
       viewModel: singleCommentViewModel,
-      template:  '<div>Hello single comment</div>'
+      // weird path with where text.js is located and how to
+      // point it to the actual template
+      template:  { require: '../../js/text!../../components/single-comment/single-comment.html' }
    };
 
 
 });
-
-
-
-
-
-
-
--- js/index.js
+++ js/index.js
@@ -1,9 +1,47 @@
-requirejs(["knockout/knockout-3.4.0", "../components/single-comment/singleComment"], function (ko) {
+require.config( {
+    baseUrl : '',
+    paths : {
+        components : 'components',
+        text: 'js/text'
+    }
+});
+
+requirejs(["../../js/knockout/knockout-3.4.0", "../../js/text"], function (ko) {
 
    console.log(ko);
    
    function indexViewModel() {
       this.num = 4;
+      this.comments = ko.observableArray([
+         {
+            username: 'user1',
+            commentText: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
+            date: new Date().toLocaleString(),
+            score: ko.observable(3),
+            voteVisible: ko.observable(false)
+         },
+         {
+            username: 'user000',
+            commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            date: new Date().toLocaleString(),
+            score: ko.observable(0),
+            voteVisible: ko.observable(false)
+         },
+         {
+            username: 'user2',
+            commentText: 'this is another good comment',
+            date: new Date().toLocaleString(),
+            score: ko.observable(-5),
+            voteVisible: ko.observable(false)
+         },
+         {
+            username: 'user3',
+            commentText: 'fourth comment',
+            date: new Date().toLocaleString(),
+            score: ko.observable(19),
+            voteVisible: ko.observable(false)
+         }
+      ]);
    }
 
    ko.components.register('single-comment', { require: '../../components/single-comment/singleComment' });
